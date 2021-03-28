f2a23e59a644f9f415074c80bf9f41fe409d619a
Bilawal Hassan <bilawal.hassan@confiz.local>
2021-03-28 20:22:22 +0500

    Fixed indentation and added Question

--- Lesson 10/MinPerimeterRectangle.java
+++ Lesson 10/MinPerimeterRectangle.java
@@ -1,6 +1,36 @@
 import java.util.ArrayList;
 import java.util.List;
 
+/*
+
+An integer N is given, representing the area of some rectangle.
+
+The area of a rectangle whose sides are of length A and B is A * B, and the perimeter is 2 * (A + B).
+
+The goal is to find the minimal perimeter of any rectangle whose area equals N. The sides of this rectangle should be only integers.
+
+For example, given integer N = 30, rectangles of area 30 are:
+
+(1, 30), with a perimeter of 62,
+(2, 15), with a perimeter of 34,
+(3, 10), with a perimeter of 26,
+(5, 6), with a perimeter of 22.
+Write a function:
+
+class Solution { public int solution(int N); }
+
+that, given an integer N, returns the minimal perimeter of any rectangle whose area is exactly equal to N.
+
+For example, given an integer N = 30, the function should return 22, as explained above.
+
+Write an efficient algorithm for the following assumptions:
+
+N is an integer within the range [1..1,000,000,000].
+Copyright 2009–2021 by Codility Limited. All Rights Reserved. Unauthorized copying, publication or disclosure prohibited.
+
+https://app.codility.com/programmers/lessons/10-prime_and_composite_numbers/min_perimeter_rectangle/
+ */
+
 public class MinPerimeterRectangle {
 
     public int solution(int N) {
@@ -21,7 +51,8 @@ public class MinPerimeterRectangle {
                 }
             }
         }
-        return minPerimeter;
+        
+	return minPerimeter;
 
     }
     private List<Integer> factors(int n) {
@@ -33,7 +64,8 @@ public class MinPerimeterRectangle {
                     factors.add(n/i);
             }
         }
-        return factors;
+        
+	return factors;
     }
     public static void main(String args[]){
         MinPerimeterRectangle obj = new MinPerimeterRectangle();
